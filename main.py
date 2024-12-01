fd53ef985768b7281613c05b76da3b09b3850cb4
varushchi <vadim.shchigolev#mail.ru>
2024-12-01 18:53:04 +0300

    done

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-# тренировка
-model = YOLO("yolov8n-seg.pt")
-result = model.train(data = "config.yaml", epochs=300)
+# # тренировка
+# model = YOLO("yolov8n-seg.pt")
+# result = model.train(data = "config.yaml", epochs=300)
 
-# # тестирование
-# model = YOLO("runs/segment/train2/weights/best.pt")
-# result = model.predict(source = 'data/train/images/8.png', conf = 0.25, save = True, hide_labels = True, boxes=False)
+# тестирование
+model = YOLO("runs/segment/train5/weights/best.pt")
+result = model.predict(source = 'image_20240816_215029_043858.png', conf = 0.1, save = True, hide_labels = True, boxes=False)
